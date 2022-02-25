26d8f5aac14bdd2d2f44463d6d3321bad5b7f510
nayruthCalla <nayruth.calla@gmail.com>
2022-02-24 22:55:24 -0500

    fix: configure apollo client

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -3,7 +3,10 @@ import { setContext } from '@apollo/client/link/context'
 
 // process.env.REACT_APP_BACKEND_URL ||
 const httpLink = createHttpLink({
-  uri: process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000/graphql',
+  uri: 'http://localhost:4000/graphql',
+  // fetchOptions: {
+  //   mode: 'no-cors',
+  // },
 })
 
 const authLink = setContext((_, { headers }) => {
